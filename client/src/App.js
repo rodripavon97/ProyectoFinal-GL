e1fabde381ebc1e0b4b495b5fee3695314ba69cc
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2022-08-01 19:01:13 -0300

    cambios minimos en login

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Login from './pages/Login/Login';
-import Footer from './components/Footer/Footer';
+
 function App() {
   return (
     <div className="App">
       <Login/>
-      <Footer/>
+      
     </div>
   );
 }
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,67 +1,75 @@
 import * as React from "react";
-import {
-  TextField,
-  Grid,
-  Typography,
-  ButtonGroup,
-  Button,
-} from "@mui/material";
+import { TextField, Grid, Typography, Button, Box } from "@mui/material";
+import Footer from '../../components/Footer/Footer';
 
 const Login = () => {
   return (
-    <Grid
-      container
-      spacing={2}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: "100vh" }}
-    >
-      <Grid item xs={3}>
-        <Typography variant="h3">Login</Typography>
-        <TextField
-          id="filled-basic"
-          label="Email"
-          variant="filled"
-          fullWidth
-          style={{ marginBottom: "2em" }}
-          sx={{
-            input: {
-              backgroundColor: "black",
-              color: "#0fb66e",
-            },
-            label: {
-              color: "#02a663",
-            },
-          }}
-        />
+    <div>
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Typography sx={{ color: "#0fb66e" }} variant="h2">
+          Login
+        </Typography>
+        <Grid item xs={3}>
+          <TextField
+            color="success"
+            id="filled-basic"
+            label="Email"
+            variant="filled"
+            fullWidth
+            style={{ marginBottom: "2em" }}
+            sx={{
+              input: {
+                backgroundColor: "#0a0a0a",
+                color: "#0fb66e",
+              },
+              label: {
+                color: "#02a663",
+              },
+            }}
+          />
 
-        <TextField
-          label="Password"
-          fullWidth
-          id="outlined-basic"
-          variant="filled"
-          style={{ marginBottom: "2em" }}
-          sx={{
-            input: {
-              backgroundColor: "black",
-              color: "#0fb66e",
-            },
-            label: {
-              color: "#02a663",
-            },
-          }}
-        />
+          <TextField
+            color="success"
+            label="Password"
+            fullWidth
+            id="outlined-basic"
+            variant="filled"
+            style={{ marginBottom: "2em" }}
+            sx={{
+              input: {
+                backgroundColor: "#0a0a0a",
+                color: "#0fb66e",
+              },
+              label: {
+                color: "#02a663",
+              },
+            }}
+          />
+        </Grid>
+        <Box m={1} pt={1}>
+          <Button
+            sx={{ margin: 1 }}
+            style={{ backgroundColor: "#0fb66e", color: "#000000" }}
+          >
+            Sign In
+          </Button>
+          <Button
+            sx={{ margin: 2 }}
+            style={{ backgroundColor: "#02a663", color: "#000000" }}
+          >
+            Sign Up
+          </Button>
+        </Box>
       </Grid>
-      <ButtonGroup variant="contained">
-        <Button
-         style = {{ backgroundColor:"#0fb66e", color:"#000000"}}
-        >
-          Sign In
-        </Button>
-        <Button style = {{ backgroundColor:"#02a663", color:"#000000"}}>Sign Up</Button>
-      </ButtonGroup>
-    </Grid>
+      <Footer/>
+    </div>
   );
 };
 export default Login;
